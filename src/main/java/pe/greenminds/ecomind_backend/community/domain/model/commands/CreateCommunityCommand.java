ceb02bd7668e83bbebe360dd0697ef300fdce3a3
Leotens <leotens11@gmail.com>
2026-06-13 16:48:41 -0500

    feat: Add community domain

---
+++ src/main/java/pe/greenminds/ecomind_backend/community/domain/model/commands/CreateCommunityCommand.java
@@ -0,0 +1,8 @@
+package pe.greenminds.ecomind_backend.community.domain.model.commands;
+
+public record CreateCommunityCommand(
+        String name,
+        Integer userCount,
+        String location
+) {
+}
