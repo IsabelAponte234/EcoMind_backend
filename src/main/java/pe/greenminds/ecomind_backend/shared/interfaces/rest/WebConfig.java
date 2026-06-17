563d4a9119c08bfb27d2a0d7a1aa087c97f4928d
Zatrynox <vcristianlaura20@gmail.com>
2026-06-17 01:49:24 -0500

    feat: add COORS for deploy

---
+++ src/main/java/pe/greenminds/ecomind_backend/shared/interfaces/rest/WebConfig.java
@@ -0,0 +1,18 @@
+package pe.greenminds.ecomind_backend.shared.interfaces.rest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://ecomind-app.web.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
